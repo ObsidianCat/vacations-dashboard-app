00d84f69f26a014f99d0d46881c2c025a425d02c
Lula Leus <lula.leus@gmail.com>
2016-11-06 19:48:32 +0200

    Feat: Adding Material-UI

--- src/components/App.js
+++ src/components/App.js
@@ -9,25 +9,27 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 const App = (props) =>{
   return (
-    <div className="wrap container-fluid mdl-layout mdl-js-layout mdl-layout--fixed-header">
-      <header className="row mdl-layout__header">
-        <div className="mdl-layout__header-row">
-          <IndexLink to="/"><span className="mdl-layout-title">Time travel vacations dashboard </span></IndexLink>
-          <div className="mdl-layout-spacer"></div>
-          <nav className="mdl-navigation mdl-layout--large-screen-only">
-            <p className="mdl-navigation__link">
-              <Link to="/edit">Edit content</Link>
-            </p>
-            <p className="mdl-navigation__link" >
-              <Link to="/create">Create content</Link>
-            </p>
-          </nav>
-        </div>
-      </header>
-      <main className="page-section mdl-layout__content">
-        {props.children}
-      </main>
-    </div>
+    <MuiThemeProvider>
+      <div className="wrap container-fluid mdl-layout mdl-js-layout mdl-layout--fixed-header">
+        <header className="row mdl-layout__header">
+          <div className="mdl-layout__header-row">
+            <IndexLink to="/"><span className="mdl-layout-title">Time travel vacations dashboard </span></IndexLink>
+            <div className="mdl-layout-spacer"></div>
+            <nav className="mdl-navigation mdl-layout--large-screen-only">
+              <p className="mdl-navigation__link">
+                <Link to="/edit">Edit content</Link>
+              </p>
+              <p className="mdl-navigation__link" >
+                <Link to="/create">Create content</Link>
+              </p>
+            </nav>
+          </div>
+        </header>
+        <main className="page-section mdl-layout__content">
+          {props.children}
+        </main>
+      </div>
+    </MuiThemeProvider>
   );
 };
 
