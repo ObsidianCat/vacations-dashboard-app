61208c8e2850f9b7a67a4922e67a9a169980de32
Lula Leus <lula.leus@gmail.com>
2016-11-13 19:01:00 +0200

    Feat: Destination updating form

--- src/components/DestinationForm.js
+++ src/components/DestinationForm.js
@@ -21,51 +21,53 @@ class DestinationForm extends React.Component{
     this.setState({
       value: event.target.value,
     });
+
+    const field = event.target.name;
+    let destination = this.state.destination;
+    destination[field] = event.target.value;
+    return this.setState({destination});
+
   };
 
 
   componentWillReceiveProps(nextProps) {
-  console.log('componentWillReceiveProps', nextProps);
-    // if(this.props.selectedDestination.placeName !=''){
+    if(this.props.selectedDestination._id != nextProps.selectedDestination._id){
       this.setState({
-        destination: nextProps.selectedDestination,
+        destination:Object.assign({}, nextProps.selectedDestination)
       });
-    // };
+    }
   }
 
 
 
-  createArtTagsList = (destination)=>{
-    const tags = DESTINATION_DATA.ART_TAGS.map(function(tag) {
-      console.log('artTagsList');
-      debugger;
+  createTagsList = (destinationTags, allTags)=>{
+    const onChangeCallback = this.handleChange;
+    const tags = allTags.map(function(tag, onChangeCallback) {
+      let isChecked = _.find(destinationTags, function(o) { return o.name === tag.name; });
+      isChecked = isChecked !=undefined;
+      console.log(this);
 
       return (
         <Checkbox
           key={tag.name}
           label={tag.name}
+          defaultChecked={isChecked}
+          name={tag.name}
+          onChange={onChangeCallback}
         />
       );
+
     });
     return tags;
   };
-
-  historyTagsList = DESTINATION_DATA.HISTORY_TAGS.map(function(tag) {
-    return (
-      <Checkbox
-        key={tag.name}
-        label={tag.name}
-      />
-    );
-  });
-
 
   render(){
     if(!this.state.destination.placeName){
       return null;
     } else{
 
-      const artTagsList = this.createArtTagsList(this.state.destination);
+      const artTagsList = this.createTagsList(this.state.destination.artTags, DESTINATION_DATA.ART_TAGS);
+      const historyTagsList = this.createTagsList(this.state.destination.historyTags, DESTINATION_DATA.HISTORY_TAGS);
 
       return (
         <form action="#">
@@ -76,21 +78,23 @@ class DestinationForm extends React.Component{
               id="destination-name-field"
               value={this.state.destination.placeName||''}
               onChange={this.handleChange}
+              name="placeName"
             /><br/>
             <TextField
               hintText="Country Name"
               floatingLabelText="Country Name"
               id="destination-name-field"
               value={this.state.destination.countryName||''}
-              onChange={this.handleChange}/><br/>
+              onChange={this.handleChange}
+              name="countryName"/><br/>
           </fieldset>
           <fieldset className="art-tags">
             <legend>Art Tags</legend>
-            {this.artTagsList}
+            {artTagsList}
           </fieldset>
           <fieldset className="history-tags">
             <legend>History Tags</legend>
-            {this.historyTagsList}
+            {historyTagsList}
 
           </fieldset>
 
