e5346500625d75d955ceed47913d35d11ed15648
Lula Leus <lula.leus@gmail.com>
2016-12-13 15:28:17 +0200

    Feat: Showing recent activities on home page

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,24 +2,25 @@
  * Created by Lula on 10/18/2016.
  */
 import React, {PropTypes} from 'react';
-
 import {connect} from 'react-redux';
-
-// class HomePage extends React.Component{
-//   render(){
-//     return (
-//       <div>
-//         <h2>Recent actions</h2>
-//       </div>
-//     )
-//   }
-// }
+import moment from 'moment';
+moment().format();
 
 export const HomePage = (props) => {
-  console.log('activities', props.activities);
+  const activities = props.activities.map((activity, index) => {
+    let activityDate = moment(activity.date).format("dddd, MMMM Do YYYY, h:mm:ss a");
+    return <li className="recentActivitiesList--item mdl-list__item" key={index}>
+      <span className="recentActivitiesList--date">{activityDate}</span>
+      <span className="recentActivitiesList--name">{activity.message}</span>
+    </li>;
+  });
+
   return (
-    <div>
+    <div className="homePage">
       <h2>Recent actions</h2>
+      <ul className="recentActivitiesList">
+        {activities}
+      </ul>
     </div>
   );
 };
